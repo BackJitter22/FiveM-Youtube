f9e697b1684d82243d75930c0f178169732f740b
Gaklio <gaklio2321@gmail.com>
2022-01-03 17:29:44 -0500

    Player Identifiers Inserted into a database
    Episode one of the YT Series, inserting fivem values into a database for future uses.

---
+++ EpisodeOne/fxmanifest.lua
@@ -0,0 +1,11 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'Gaklio'
+description 'MySQL Async Video 1'
+version '0.0.1'
+
+server_scripts{
+    'server/main.lua',
+    '@mysql-async/lib/MySQL.lua'
+}
